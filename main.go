271939f86be1a1b5221d89eacc2baec87ce5064b
Christian Kniep <christian@qnib.org>
2018-10-21 22:01:11 +0200

    Release version 0.1.9

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Frontend to spawn Jupyter Notebooks."
 	app.Usage = "jupyterport [options]"
-	app.Version = "0.1.8"
+	app.Version = "0.1.9"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "listen-addr",
